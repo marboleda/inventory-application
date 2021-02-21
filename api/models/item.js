6efc555ee79c2b73c9dfe1de09551f58aeaa6442
Marco Arboleda <arboleda.marco@gmail.com>
2021-02-21 11:36:30 -0500

    Add oz as valid weight unit for item

--- api/models/item.js
+++ api/models/item.js
@@ -6,7 +6,7 @@ const ItemSchema = new Schema(
     {
         name: {type: String, required: true, maxlength: 100},
         weight_num: {type: Number},
-        weight_unit: {type: String, enum: ['kg','g','ml','l']},
+        weight_unit: {type: String, enum: ['kg','g','ml','l', 'oz']},
         price: {type: Number},
         category: {type: Schema.Types.ObjectId, ref: 'Category'},
         stock: {type: Number},
