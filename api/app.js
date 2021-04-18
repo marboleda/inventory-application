8a13ae6744b4d15f0aa37691df7b53362cb29934
Marco Arboleda <arboleda.marco@gmail.com>
2021-04-18 15:55:13 -0400

    Add Multer to api

--- api/app.js
+++ api/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const multer = require('multer');
+const path = require('path');
 
 
 const indexRouter = require('./routes/index');
